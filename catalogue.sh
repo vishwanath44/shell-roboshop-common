2ac8595eb145f964ecd1f8a582eb0c8fd28de98b
vishwanath44 <chinthavishwanath9@gmail.com>
2026-02-08 12:52:38 +0530

    added new com

---
+++ catalogue.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+source ./common.sh
+app_name=catalogue
+
+check_root
+app_setup
+nodejs_setup
+systemd_steup
+
+# Loading data into MongoDB
+cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
+dnf install mongodb-mongosh -y &>>$LOGS_FILE
+
+INDEX=$(mongosh --host $MONGODB_HOST --quiet  --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
+
+if [ $INDEX -le 0 ]; then
+    mongosh --host $MONGODB_HOST </app/db/master-data.js
+    VALIDATE $? "Loading products"
+else
+    echo -e "$(date "+%Y-%M-%d %H:%M:%S") | Products already loaded ... $Y SKIPPING $N"
+fi
+
+systemctl restart catalogue
+VALIDATE $? "Restarting catalogue"
+
+
+
