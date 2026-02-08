7510f9cb51c4f42641950be789423cfab7826afe
vishwanath44 <chinthavishwanath9@gmail.com>
2026-02-08 13:43:15 +0530

    added new com

---
+++ shipping.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+source ./common.sh
+app_name=catalogue
+
+check_root
+app_setup
+java_setup
+systemd_setup
+
+dnf install mysql -y  &>>$LOGS_FILE
+VALIDATE $? "Installing MySQL"
+
+mysql -h $MYSQL_HOST -uroot -pRoboShop@1 -e 'use cities'
+if [ $? -ne 0 ]; then
+
+    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql &>>$LOGS_FILE
+    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/app-user.sql &>>$LOGS_FILE
+    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$LOGS_FILE
+    VALIDATE $? "Loaded data into MySQL"
+else
+    echo -e "data is already loaded ... $Y SKIPPING $N"
+fi
+
+app_restart
+print_total_time
